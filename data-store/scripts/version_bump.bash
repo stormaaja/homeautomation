e094e8fc0beda7bb0aabd307be2b3dcb692dac02
Matti Ahinko <matti.ahinko@gmail.com>
2024-11-30 10:45:20 +0200

    Update version with action

---
+++ data-store/scripts/version_bump.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+version_file="version.go"
+
+current_version=$(perl -nle 'print $1 if /Version = "([^"]+)"/' $version_file)
+
+echo "Current version: $current_version"
+
+# Increment the version
+build_version=$(echo $current_version | cut -d. -f3)
+new_build_version=$(($build_version + 1))
+new_version=$(echo $current_version | cut -d. -f1-2).$new_build_version
+
+echo "New version: $new_version"
+
+# Update the version in the version file
+perl -pi -e "s/Version = \"$current_version\"/Version = \"$new_version\"/" $version_file
