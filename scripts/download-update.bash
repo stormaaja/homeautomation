300123e4a3a08c683f807361e02cf6d3a4cd30e3
Matti Ahinko <matti.ahinko@gmail.com>
2024-12-03 15:29:09 +0200

    Add symlink for latest

--- scripts/download-update.bash
+++ scripts/download-update.bash
@@ -10,3 +10,5 @@ fi
 # Download the new version
 curl "https://github.com/stormaaja/homeautomation/releases/download/v$version/data-store-v$version-linux-amd64" -LO
 chmod u+x data-store-v$version-linux-amd64
+rm data-store-latest
+ln -s data-store-v$version-linux-amd64 data-store-latest
