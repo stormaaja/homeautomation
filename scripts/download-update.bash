b193126043c15ff6023c90e462c2401ca547f87d
Matti Ahinko <matti.ahinko@gmail.com>
2024-12-03 15:21:47 +0200

    Add downlod update script

---
+++ scripts/download-update.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+version=$1
+
+if [ -z "$version" ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+# Download the new version
+curl "https://github.com/stormaaja/homeautomation/releases/download/v$version/data-store-v$version-linux-amd64" -LO
+chmod u+x data-store-v$version-linux-amd64
