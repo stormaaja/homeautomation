fc6f2cc24c36ce8fc0190e3657ba5d6054506b37
Matti Ahinko <matti.ahinko@gmail.com>
2024-12-03 09:10:51 +0200

    Change to more clean variable validator

--- security/configvalidators/env_validator.go
+++ security/configvalidators/env_validator.go
@@ -5,13 +5,19 @@ import (
 	"os"
 )
 
+var requiredEnvironmentVariables = []string{
+	"API_TOKEN",
+	"PORT",
+}
+
 // IsConfigEnvironmentVariablesValid validates environment variables
 //
 // It returns error, if the validation is failed. Otherwise, it returns nil.
 func IsConfigEnvironmentVariablesValid() error {
-	apiToken := os.Getenv("API_TOKEN")
-	if apiToken == "" {
-		return errors.New("API_TOKEN is not set")
+	for _, environmentVariable := range requiredEnvironmentVariables {
+		if os.Getenv(environmentVariable) == "" {
+			return errors.New(environmentVariable + " is not set")
+		}
 	}
 	return nil
 }
