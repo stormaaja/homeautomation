651aeed1dafc21de2e12c89adb75f68e338de40c
Matti Ahinko <matti.ahinko@gmail.com>
2025-04-26 08:57:38 +0300

    Test miners routes

--- data-store/assert/assert.go
+++ data-store/assert/assert.go
@@ -1,6 +1,10 @@
 package assert
 
-import "testing"
+import (
+	"encoding/json"
+	"maps"
+	"testing"
+)
 
 func True(
 	t *testing.T,
@@ -23,6 +27,39 @@ func Equal(
 	if expected != actual {
 		t.Errorf(
 			"Assertion failed. Expected %v, got %v",
+			expected,
+			actual,
+		)
+	}
+}
+
+func JSONEq(
+	t *testing.T,
+	expected string,
+	actual string,
+) {
+	t.Helper()
+	parsedTarget := map[string]interface{}{}
+	parsedActual := map[string]interface{}{}
+	err := json.Unmarshal([]byte(expected), &parsedTarget)
+	if err != nil {
+		t.Errorf(
+			"Failed to parse expected JSON: %v",
+			err,
+		)
+		return
+	}
+	err = json.Unmarshal([]byte(actual), &parsedActual)
+	if err != nil {
+		t.Errorf(
+			"Failed to parse actual JSON: %v",
+			err,
+		)
+		return
+	}
+	if !maps.Equal(parsedTarget, parsedActual) {
+		t.Errorf(
+			"Assertion failed. Expected JSON %s, got %s",
 			expected,
 			actual,
 		)
